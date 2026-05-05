ef857aa7ab9a78785fd73d8428e8c68b6afea279
Sander Goppel <website@vertero.nl>
2026-05-05 17:48:50 +0200

    fix: inline styles on powered-by logo to override host CSS

--- public/widget.js
+++ public/widget.js
@@ -127,7 +127,7 @@
       <div id="vertero-modal">
         <button id="vertero-close">✕</button>
         <div id="vertero-content"></div>
-        <div class="vertero-powered"><span>Powered by</span><a href="https://vertero.nl" target="_blank" rel="noopener noreferrer"><img src="${apiBase}/logo.png" alt="Vertero" /></a></div>
+        <div class="vertero-powered"><span>Powered by</span><a href="https://vertero.nl" target="_blank" rel="noopener noreferrer"><img src="${apiBase}/logo.png" alt="Vertero" style="height:18px!important;width:auto!important;display:inline!important;max-width:none!important;" /></a></div>
       </div>
     </div>
   `);
